b621930bd2582a64585d18eca3a9715a3f9aa459
Joan Ekajeh <you@example.com>
2024-05-13 16:21:37 +0100

    Putting the code to check guess and check for valid input into functions

---
+++ milestone_3.py
@@ -0,0 +1,30 @@
+import random
+
+word_list=['strawberry', 'mango', 'kiwi', 'orange', 'peach']
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+#check whether the guess is in the word
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess!{guess} is in the word")
+    else:
+        print(f"Sorry, {guess} is not i n the word. Try again.")
+
+#check if input is valid
+def ask_for_input():
+    while True:
+            try:
+                guess = input('Guess a letter: ')
+                if guess==1 and guess.isalpha():
+                    break
+            except:
+                print('Invalid letter. Please, enter a single alphabetical character.')
+
+    check_guess(guess)
+
+ask_for_input()
+
